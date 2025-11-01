bab82d337a40620973c1d5d3d9dc633c9d011392
justerik <sebo.erik@gmail.com>
2025-11-01 11:14:36 +0100

    hammers are now not stackable, and they have their own durability, mining speed and mining level

---
+++ src/main/java/net/withrage/simplehammers/item/custom/HammerItem.java
@@ -0,0 +1,17 @@
+package net.withrage.simplehammers.item.custom;
+
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.tag.BlockTags;
+
+public class HammerItem extends MiningToolItem {
+    public HammerItem(ToolMaterial material, Settings settings) {
+        super(
+                5.0f,
+                -3.2f,
+                material,
+                BlockTags.PICKAXE_MINEABLE,
+                settings
+        );
+    }
+}
--- src/main/java/net/withrage/simplehammers/item/custom/ModItems.java
+++ src/main/java/net/withrage/simplehammers/item/custom/ModItems.java
@@ -8,14 +8,14 @@ import net.minecraft.util.Identifier;
 import net.withrage.simplehammers.SimpleHammers;
 
 public class ModItems {
-    public static final Item WOODEN_HAMMER = registerItem("wooden_hammer", new Item(new FabricItemSettings()));
-    public static final Item STONE_HAMMER = registerItem("stone_hammer", new Item(new FabricItemSettings()));
-    public static final Item COPPER_HAMMER = registerItem("copper_hammer", new Item(new FabricItemSettings()));
-    public static final Item GOLDEN_HAMMER = registerItem("golden_hammer", new Item(new FabricItemSettings()));
-    public static final Item IRON_HAMMER = registerItem("iron_hammer", new Item(new FabricItemSettings()));
-    public static final Item EMERALD_HAMMER = registerItem("emerald_hammer", new Item(new FabricItemSettings()));
-    public static final Item DIAMOND_HAMMER = registerItem("diamond_hammer", new Item(new FabricItemSettings()));
-    public static final Item NETHERITE_HAMMER = registerItem("netherite_hammer", new Item(new FabricItemSettings()));
+    public static final Item WOODEN_HAMMER = registerItem("wooden_hammer", new HammerItem(ModToolMaterial.WOOD, new FabricItemSettings().maxCount(1).maxDamage(59)));
+    public static final Item STONE_HAMMER = registerItem("stone_hammer", new HammerItem(ModToolMaterial.STONE, new FabricItemSettings().maxCount(1).maxDamage(131)));
+    public static final Item COPPER_HAMMER = registerItem("copper_hammer", new HammerItem(ModToolMaterial.COPPER, new FabricItemSettings().maxCount(1).maxDamage(200)));
+    public static final Item GOLDEN_HAMMER = registerItem("golden_hammer", new HammerItem(ModToolMaterial.GOLD, new FabricItemSettings().maxCount(1).maxDamage(32)));
+    public static final Item IRON_HAMMER = registerItem("iron_hammer", new HammerItem(ModToolMaterial.IRON, new FabricItemSettings().maxCount(1).maxDamage(250)));
+    public static final Item EMERALD_HAMMER = registerItem("emerald_hammer", new HammerItem(ModToolMaterial.EMERALD, new FabricItemSettings().maxCount(1).maxDamage(1400)));
+    public static final Item DIAMOND_HAMMER = registerItem("diamond_hammer", new HammerItem(ModToolMaterial.DIAMOND, new FabricItemSettings().maxCount(1).maxDamage(1561)));
+    public static final Item NETHERITE_HAMMER = registerItem("netherite_hammer", new HammerItem(ModToolMaterial.NETHERITE, new FabricItemSettings().maxCount(1).maxDamage(2031)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(SimpleHammers.MOD_ID, name), item);
