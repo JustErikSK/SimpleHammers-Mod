d619d8c16d5aed549feddd9eeb23b7d23d03ef51
justerik <sebo.erik@gmail.com>
2025-11-01 09:40:35 +0100

    completely changed textures for all hammers, added copper hammer and emerald hammer

--- src/main/java/net/withrage/simplehammers/item/custom/ModItems.java
+++ src/main/java/net/withrage/simplehammers/item/custom/ModItems.java
@@ -10,8 +10,10 @@ import net.withrage.simplehammers.SimpleHammers;
 public class ModItems {
     public static final Item WOODEN_HAMMER = registerItem("wooden_hammer", new Item(new FabricItemSettings()));
     public static final Item STONE_HAMMER = registerItem("stone_hammer", new Item(new FabricItemSettings()));
+    public static final Item COPPER_HAMMER = registerItem("copper_hammer", new Item(new FabricItemSettings()));
     public static final Item GOLDEN_HAMMER = registerItem("golden_hammer", new Item(new FabricItemSettings()));
     public static final Item IRON_HAMMER = registerItem("iron_hammer", new Item(new FabricItemSettings()));
+    public static final Item EMERALD_HAMMER = registerItem("emerald_hammer", new Item(new FabricItemSettings()));
     public static final Item DIAMOND_HAMMER = registerItem("diamond_hammer", new Item(new FabricItemSettings()));
     public static final Item NETHERITE_HAMMER = registerItem("netherite_hammer", new Item(new FabricItemSettings()));
 
