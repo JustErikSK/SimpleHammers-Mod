3b075f6a623b577f210448367789da704e2c6da8
justerik <sebo.erik@gmail.com>
2025-11-04 16:57:35 +0100

    small changes to the base of the mod

---
+++ src/main/java/net/withrage/simplehammers/mixin/HammerMixin.java
@@ -0,0 +1,8 @@
+package net.withrage.simplehammers.mixin;
+
+import net.minecraft.screen.AnvilScreenHandler;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(AnvilScreenHandler.class)
+public abstract class HammerMixin {
+}
