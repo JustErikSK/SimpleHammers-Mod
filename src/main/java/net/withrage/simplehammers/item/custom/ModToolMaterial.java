7b4bc14bdf5d2baf96d6a4a582874c2cbc4af217
justerik <sebo.erik@gmail.com>
2025-11-05 14:29:40 +0100

    added code, recipes, textures etc, enchants not working yet tho

---
+++ src/main/java/net/withrage/simplehammers/item/custom/ModToolMaterial.java
@@ -0,0 +1,34 @@
+package net.withrage.simplehammers.item.custom;
+
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.tag.BlockTags;
+
+public class ModToolMaterial {
+
+    public static final ToolMaterial WOOD      = ToolMaterial.WOOD;
+    public static final ToolMaterial STONE     = ToolMaterial.STONE;
+    public static final ToolMaterial IRON      = ToolMaterial.IRON;
+    public static final ToolMaterial GOLD      = ToolMaterial.GOLD;
+    public static final ToolMaterial DIAMOND   = ToolMaterial.DIAMOND;
+    public static final ToolMaterial NETHERITE = ToolMaterial.NETHERITE;
+
+    public static final ToolMaterial COPPER =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_IRON_TOOL,
+                    262,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.COPPER_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial EMERALD =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_DIAMOND_TOOL,
+                    2031,
+                    8.0f,
+                    3.0f,
+                    15,
+                    ModItemTags.EMERALD_TOOL_MATERIALS
+            );
+}
