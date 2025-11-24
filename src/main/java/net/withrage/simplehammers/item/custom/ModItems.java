7bf91e73edbd9c313e42fa2bb34c06f0ef70347a
justerik <sebo.erik@gmail.com>
2025-11-24 19:03:37 +0100

    lowered the attack damage for all hammers

--- src/main/java/net/withrage/simplehammers/item/custom/ModItems.java
+++ src/main/java/net/withrage/simplehammers/item/custom/ModItems.java
@@ -8,14 +8,14 @@ import net.minecraft.util.Identifier;
 import net.withrage.simplehammers.SimpleHammers;
 
 public class ModItems {
-    public static final Item WOODEN_HAMMER = registerItem("wooden_hammer", new HammerItem(ModToolMaterial.WOOD, 2, -2.8F, new Item.Settings().maxCount(1).maxDamage(108)));
-    public static final Item STONE_HAMMER = registerItem("stone_hammer", new HammerItem(ModToolMaterial.STONE, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(262)));
-    public static final Item COPPER_HAMMER = registerItem("copper_hammer", new HammerItem(ModToolMaterial.COPPER, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(380)));
-    public static final Item GOLDEN_HAMMER = registerItem("golden_hammer", new HammerItem(ModToolMaterial.GOLD, 2, -2.8F, new Item.Settings().maxCount(1).maxDamage(64)));
-    public static final Item IRON_HAMMER = registerItem("iron_hammer", new HammerItem(ModToolMaterial.IRON, 4, -2.8F, new Item.Settings().maxCount(1).maxDamage(506)));
-    public static final Item EMERALD_HAMMER = registerItem("emerald_hammer", new HammerItem(ModToolMaterial.EMERALD, 5, -2.8F, new Item.Settings().maxCount(1).maxDamage(2084)));
-    public static final Item DIAMOND_HAMMER = registerItem("diamond_hammer", new HammerItem(ModToolMaterial.DIAMOND, 5, -2.8F, new Item.Settings().maxCount(1).maxDamage(3122)));
-    public static final Item NETHERITE_HAMMER = registerItem("netherite_hammer", new HammerItem(ModToolMaterial.NETHERITE, 6, -2.8F, new Item.Settings().maxCount(1).maxDamage(4062)));
+    public static final Item WOODEN_HAMMER = registerItem("wooden_hammer", new HammerItem(ModToolMaterial.WOOD, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(108)));
+    public static final Item STONE_HAMMER = registerItem("stone_hammer", new HammerItem(ModToolMaterial.STONE, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(262)));
+    public static final Item COPPER_HAMMER = registerItem("copper_hammer", new HammerItem(ModToolMaterial.COPPER, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(380)));
+    public static final Item GOLDEN_HAMMER = registerItem("golden_hammer", new HammerItem(ModToolMaterial.GOLD, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(64)));
+    public static final Item IRON_HAMMER = registerItem("iron_hammer", new HammerItem(ModToolMaterial.IRON, 2, -2.8F, new Item.Settings().maxCount(1).maxDamage(506)));
+    public static final Item EMERALD_HAMMER = registerItem("emerald_hammer", new HammerItem(ModToolMaterial.EMERALD, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(2084)));
+    public static final Item DIAMOND_HAMMER = registerItem("diamond_hammer", new HammerItem(ModToolMaterial.DIAMOND, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(3122)));
+    public static final Item NETHERITE_HAMMER = registerItem("netherite_hammer", new HammerItem(ModToolMaterial.NETHERITE, 4, -2.8F, new Item.Settings().maxCount(1).maxDamage(4062)));
 
     public static final Item HAMMER_HANDLE = registerItem("hammer_handle", new Item(new FabricItemSettings()));
     public static final Item WOODEN_HAMMER_HEAD = registerItem("wooden_hammer_head", new Item(new FabricItemSettings()));
