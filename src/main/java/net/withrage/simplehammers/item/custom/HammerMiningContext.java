ca5121fddff9d2574621bf683eb75f5b8b3fc71b
justerik <sebo.erik@gmail.com>
2026-02-22 23:41:50 +0100

    ported v1.0.2 to 1.21.10

--- src/main/java/net/withrage/simplehammers/item/custom/HammerMiningContext.java
+++ src/main/java/net/withrage/simplehammers/item/custom/HammerMiningContext.java
@@ -6,13 +6,10 @@ import java.util.WeakHashMap;
 import java.util.Map;
 
 public class HammerMiningContext {
-
     private static final Map<PlayerEntity, Direction> LAST_FACE = new WeakHashMap<>();
-
     public static void setLastHitFace(PlayerEntity player, Direction face) {
         LAST_FACE.put(player, face);
     }
-
     public static Direction consumeLastHitFace(PlayerEntity player) {
         return LAST_FACE.remove(player);
     }
--- src/main/java/net/withrage/simplehammers/item/custom/ModItemTags.java
+++ src/main/java/net/withrage/simplehammers/item/custom/ModItemTags.java
@@ -6,7 +6,21 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public final class ModItemTags {
+    public static final TagKey<Item> WOODEN_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simplehammers", "wooden_tool_materials"));
+    public static final TagKey<Item> STONE_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simplehammers", "stone_tool_materials"));
+    public static final TagKey<Item> COPPER_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simplehammers", "copper_tool_materials"));
+    public static final TagKey<Item> GOLDEN_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simplehammers", "golden_tool_materials"));
+    public static final TagKey<Item> IRON_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simplehammers", "iron_tool_materials"));
     public static final TagKey<Item> EMERALD_TOOL_MATERIALS =
             TagKey.of(RegistryKeys.ITEM, Identifier.of("simplehammers", "emerald_tool_materials"));
+    public static final TagKey<Item> DIAMOND_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simplehammers", "diamond_tool_materials"));
+    public static final TagKey<Item> NETHERITE_TOOL_MATERIALS =
+            TagKey.of(RegistryKeys.ITEM, Identifier.of("simplehammers", "netherite_tool_materials"));
     private ModItemTags() {}
 }
