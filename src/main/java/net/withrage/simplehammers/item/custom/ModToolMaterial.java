21e1d1438f301a54ee67c18895aa92d4880ecdc2
justerik <sebo.erik@gmail.com>
2026-02-22 23:25:22 +0100

    ported v1.0.2 to 1.21.6

--- src/main/java/net/withrage/simplehammers/item/custom/ModToolMaterial.java
+++ src/main/java/net/withrage/simplehammers/item/custom/ModToolMaterial.java
@@ -2,32 +2,86 @@ package net.withrage.simplehammers.item.custom;
 
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.registry.tag.BlockTags;
+import net.withrage.simplehammers.config.SimpleHammersConfig;
 
 public class ModToolMaterial {
-    public static final ToolMaterial WOOD = ToolMaterial.WOOD;
-    public static final ToolMaterial STONE = ToolMaterial.STONE;
-    public static final ToolMaterial GOLD = ToolMaterial.GOLD;
-    public static final ToolMaterial IRON = ToolMaterial.IRON;
-    public static final ToolMaterial DIAMOND = ToolMaterial.DIAMOND;
-    public static final ToolMaterial NETHERITE = ToolMaterial.NETHERITE;
+    public static final ToolMaterial WOOD =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_WOODEN_TOOL,
+                    SimpleHammersConfig.woodenDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.WOODEN_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial STONE =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_STONE_TOOL,
+                    SimpleHammersConfig.stoneDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.STONE_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial COPPER =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_IRON_TOOL,
+                    SimpleHammersConfig.copperDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.COPPER_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial GOLD =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_GOLD_TOOL,
+                    SimpleHammersConfig.goldenDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.GOLDEN_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial IRON =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_IRON_TOOL,
+                    SimpleHammersConfig.ironDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.IRON_TOOL_MATERIALS
+            );
 
     public static final ToolMaterial EMERALD =
             new ToolMaterial(
                     BlockTags.INCORRECT_FOR_DIAMOND_TOOL,
-                    2031,
+                    SimpleHammersConfig.emeraldDurability,
                     8.0f,
                     3.0f,
                     15,
                     ModItemTags.EMERALD_TOOL_MATERIALS
             );
 
-    public static final ToolMaterial COPPER =
+    public static final ToolMaterial DIAMOND =
             new ToolMaterial(
-                    BlockTags.INCORRECT_FOR_IRON_TOOL,
-                    262,
+                    BlockTags.INCORRECT_FOR_DIAMOND_TOOL,
+                    SimpleHammersConfig.diamondDurability,
                     6.0f,
                     2.0f,
                     12,
-                    ModItemTags.COPPER_TOOL_MATERIALS
+                    ModItemTags.DIAMOND_TOOL_MATERIALS
+            );
+
+    public static final ToolMaterial NETHERITE =
+            new ToolMaterial(
+                    BlockTags.INCORRECT_FOR_NETHERITE_TOOL,
+                    SimpleHammersConfig.netheriteDurability,
+                    6.0f,
+                    2.0f,
+                    12,
+                    ModItemTags.NETHERITE_TOOL_MATERIALS
             );
 }
