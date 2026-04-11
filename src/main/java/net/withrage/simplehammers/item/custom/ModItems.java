853177e86153cff05cf052b83787d938c370ce5a
justerik <sebo.erik@gmail.com>
2026-04-11 18:35:42 +0200

    updated license, changed the advancement frame for Master Smith advancement, fixed an issue where all hammers have the same attack speed, fixed an issue where hammers are not mining ores properly while mining blocks with less hardness, fixed an issue where hammers are not mining properly with silk touch, version is now 1.0.4

--- src/main/java/net/withrage/simplehammers/item/custom/ModItems.java
+++ src/main/java/net/withrage/simplehammers/item/custom/ModItems.java
@@ -9,14 +9,14 @@ import net.withrage.simplehammers.SimpleHammers;
 import net.withrage.simplehammers.config.SimpleHammersConfig;
 
 public class ModItems {
-    public static final Item WOODEN_HAMMER = registerItem("wooden_hammer", new HammerItem(ModToolMaterial.WOOD, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.woodenDurability)));
-    public static final Item STONE_HAMMER = registerItem("stone_hammer", new HammerItem(ModToolMaterial.STONE, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.stoneDurability)));
-    public static final Item COPPER_HAMMER = registerItem("copper_hammer", new HammerItem(ModToolMaterial.COPPER, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.copperDurability)));
-    public static final Item GOLDEN_HAMMER = registerItem("golden_hammer", new HammerItem(ModToolMaterial.GOLD, 1, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.goldenDurability)));
-    public static final Item IRON_HAMMER = registerItem("iron_hammer", new HammerItem(ModToolMaterial.IRON, 2, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.ironDurability)));
-    public static final Item EMERALD_HAMMER = registerItem("emerald_hammer", new HammerItem(ModToolMaterial.EMERALD, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.emeraldDurability)));
-    public static final Item DIAMOND_HAMMER = registerItem("diamond_hammer", new HammerItem(ModToolMaterial.DIAMOND, 3, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.diamondDurability)));
-    public static final Item NETHERITE_HAMMER = registerItem("netherite_hammer", new HammerItem(ModToolMaterial.NETHERITE, 4, -2.8F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.netheriteDurability).fireproof()));
+    public static final Item WOODEN_HAMMER = registerItem("wooden_hammer", new HammerItem(ModToolMaterial.WOOD, 1, -3.2F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.woodenDurability)));
+    public static final Item STONE_HAMMER = registerItem("stone_hammer", new HammerItem(ModToolMaterial.STONE, 1, -3.1F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.stoneDurability)));
+    public static final Item COPPER_HAMMER = registerItem("copper_hammer", new HammerItem(ModToolMaterial.COPPER, 1, -2.9F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.copperDurability)));
+    public static final Item GOLDEN_HAMMER = registerItem("golden_hammer", new HammerItem(ModToolMaterial.GOLD, 1, -2.7F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.goldenDurability)));
+    public static final Item IRON_HAMMER = registerItem("iron_hammer", new HammerItem(ModToolMaterial.IRON, 2, -3.0F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.ironDurability)));
+    public static final Item EMERALD_HAMMER = registerItem("emerald_hammer", new HammerItem(ModToolMaterial.EMERALD, 3, -2.5F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.emeraldDurability)));
+    public static final Item DIAMOND_HAMMER = registerItem("diamond_hammer", new HammerItem(ModToolMaterial.DIAMOND, 3, -2.6F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.diamondDurability)));
+    public static final Item NETHERITE_HAMMER = registerItem("netherite_hammer", new HammerItem(ModToolMaterial.NETHERITE, 4, -2.4F, new Item.Settings().maxCount(1).maxDamage(SimpleHammersConfig.netheriteDurability).fireproof()));
 
     public static final Item WOODEN_HANDLE = registerItem("wooden_handle", new Item(new FabricItemSettings()));
     public static final Item WOODEN_HAMMER_HEAD = registerItem("wooden_hammer_head", new Item(new FabricItemSettings()));
